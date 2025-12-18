4945bc9a50c39f6a24d8add7b8a3063c08e4e05c
TaiyoYamada <tai09to06y@gmail.com>
2025-12-18 20:24:03 +0900

    フッターの幅を修正

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="py-8 text-center text-gray-400 text-sm relative z-10 glass border-t border-white/10">
+        <footer className="py-3 text-center text-gray-400 text-sm relative z-10 glass border-t border-white/10">
             <p>&copy; {new Date().getFullYear()} TaiyoYamada. All rights reserved.</p>
         </footer>
     );
